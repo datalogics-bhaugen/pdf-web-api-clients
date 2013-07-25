78c25c167c3a1d50cf5c7ac9bac45c8b11c74f7a
Felix Yen <fyen@datalogics.com>
2013-07-25 16:53:33 -0500

    use packages installed by buildout

---
+++ src/pdfprocess/setup.py
@@ -0,0 +1,8 @@
+# A minimal setup.py for development
+# See: http://www.buildout.org/docs/tutorial.html#a-minimal-develop-setup-py
+# More sophisticated examples follow that.
+from setuptools import setup
+setup(
+    name='pdfprocess',
+    package_dir={'': '.'},
+)
