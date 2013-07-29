db31be40be2767ff89ab2c28af870e4544adb64d
Felix Yen <fyen@datalogics.com>
2013-07-29 17:54:03 -0500

    LOGPATH becomes LOG_PATH

--- src/pdfprocess/pdfprocess/api_logger.py
+++ src/pdfprocess/pdfprocess/api_logger.py
@@ -11,26 +11,23 @@ def configure(app_name, logger):
 class FileHandler(logging.handlers.TimedRotatingFileHandler):
     def __init__(self, log_name, when='D', interval=1):
         'rotate daily by default'
+        self._make_formatters()
+        log_dir = os.environ['LOG_PATH'] if 'LOG_PATH' in os.environ else '.'
+        path = os.path.join(log_dir, '%s.log' % log_name)
+        super(FileHandler, self).__init__(path, when=when, interval=interval)
+    def emit(self, record):
+        self.setFormatter(self._formatters[record.levelno])
+        super(FileHandler, self).emit(record)
+    def _make_formatters(self):
         prefix = '%(asctime)s:'
         message = '%(message)s'
         level = '[%(levelname)s]'
         info_formatter = logging.Formatter(' '.join((prefix, message)))
         error_formatter = logging.Formatter(' '.join((prefix, level, message)))
-        self.formatters = {
+        self._formatters = {
             logging.DEBUG: info_formatter,
             logging.INFO: info_formatter,
             logging.WARNING: error_formatter,
             logging.ERROR: error_formatter,
             logging.CRITICAL: error_formatter}
-        log_path = os.path.join(_get_log_directory(), '%s.log' % log_name)
-        super(FileHandler, self).__init__(
-            log_path, when=when, interval=interval)
-    def emit(self, record):
-        self.setFormatter(self.formatters[record.levelno])
-        super(FileHandler, self).emit(record)
-
-def _get_log_directory():
-    'log in current working directory by default'
-    try: return os.environ['LOGPATH']
-    except KeyError: return '.'
 
