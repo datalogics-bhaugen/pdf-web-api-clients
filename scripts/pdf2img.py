70c0ad4ea0639ac5f16be22b13b43cf3218a3158
Felix Yen <fyen@datalogics.com>
2013-08-06 16:04:31 -0500

    add pdf2img.Response class

--- scripts/pdf2img.py
+++ scripts/pdf2img.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-'pdf2img emulator'
+'pdf2img driver'
 
 import glob
 import os
@@ -10,12 +10,20 @@ root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 requests_dir = glob.glob(os.path.join(root_dir, 'eggs', 'requests-*.egg'))[0]
 sys.path[0:0] = [requests_dir, os.path.join(root_dir, 'samples', 'python')]
 
-import pdf2img
+from pdf2img import PDF2IMG
 
 
 API_KEY = 'f54ab5d8-5775-42c7-b888-f074ba892b57'
 BASE_URL = 'http://127.0.0.1:5000'
 VERSION = 0
 
-pdf2img.PDF2IMG(API_KEY, VERSION, BASE_URL)()
+def run(argv):
+    pdf2img = PDF2IMG(API_KEY, VERSION, BASE_URL)
+    return pdf2img(sys.argv)
+
+if __name__ == '__main__':
+    response = run(sys.argv)
+    if not response: sys.exit(response)
+    response.save_image()
+    print('created: %s' % response.image_file)
 
