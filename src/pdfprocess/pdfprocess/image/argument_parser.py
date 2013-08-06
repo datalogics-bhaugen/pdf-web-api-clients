f94b494784fe3bbc92090287c6e77cd64172e065
Felix Yen <fyen@datalogics.com>
2013-08-06 16:01:46 -0500

    translate width/height to pixelcount

--- src/pdfprocess/pdfprocess/image/argument_parser.py
+++ src/pdfprocess/pdfprocess/image/argument_parser.py
@@ -40,32 +40,65 @@ OPTIONS = [
     Option('colorModel', '[gray|cmyk|rgb|rgba] (default=rgb)'),
     Option('compression', '[no|lzw|g3|g4|jpg] (TIFF only, default=lzw)'),
     Option('fontList', '"dir1;dir2;dirN" (see documentation for defaults)'),
-    Option('height', 'Picture height (pixels), no default'),
     Option('jpegQuality', '[1 - 100] higher values give larger file sizes'),
     Option('maxBandMem', '[1000000 - 2100000000] (default=300000000)'),
-    Option('output', '(default=input filename)'),
     Option('pages', 'comma-separated or range'),
     Option('password', '127 characters or less, no spaces'),
     Option('pdfRegion', '[crop|media|art|trim|bleed|bounding]'),
     Option('resolution', '[horiz x vert] target DPI, [12-2400] (default=300)'),
-    Option('smoothing', '[none|text|all] (default=none)'),
-    Option('width', 'Picture width (pixels), no default')]
+    Option('smoothing', '[none|text|all] (default=none)')]
+
+
+class PixelCount(object):
+    OPTIONS = [
+        Option('width', 'Picture width (pixels), no default'),
+        Option('height', 'Picture height (pixels), no default')]
+    def __init__(self):
+        self._dimensions = {}
+    def __setitem__(self, key, value):
+        self._dimensions[key.lower()] = value
+    def options(self):
+        if len(self._dimensions) == 2:
+            return ['-pixelcount=%sx%s' % (self.width, self.height)]
+        elif 'width' in self._dimensions:
+            return ['-pixelcount=w:%s' % self.width]
+        elif self._dimensions:
+            return ['-pixelcount=h:%s' % self.height]
+        else:
+            return []
+    @property
+    def width(self): return self._dimensions['width']
+    @property
+    def height(self): return self._dimensions['height']
 
 
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self):
-        argparse.ArgumentParser.__init__(self, 'pdf2img')
-        self.add_argument('inputFile', help='PDF or XPS file')
-        self.add_argument('outputForm',
-            help='EPS, TIF, JPG, BMP, PNG, GIF, RAW, or PDF')
-        for option in OPTIONS:
+        argparse.ArgumentParser.__init__(self, 'actions/image')
+        for option in OPTIONS + PixelCount.OPTIONS:
             self.add_argument('-%s' % option.name,
                 help=option.help, action=option.action)
-    def __call__(self, args=None):
-        "returns {arg: value} dict, args defaults to sys.argv[1:]"
-        return self.parse_args(args).__dict__
+    def __call__(self, request_form):
+        self._set_options(request_form)
+        self.parse_args(self.options)
     def error(self, message):
+        "overrides argparse.ArgumentParser.error"
         raise Exception(message)
-    @classmethod
-    def options(cls): return OPTIONS
+    def _set_options(self, request_form):
+        self._set_pixelcount_option(request_form)
+        for key, value in request_form.iteritems():
+            if key in OPTIONS:
+                option = OPTIONS[OPTIONS.index(key)]
+                if not isinstance(option, Flag):
+                    self._options.append('%s=%s' % (option, value))
+                elif value and value != '0' and value.lower() != 'false':
+                    self._options.append(str(option))
+    def _set_pixelcount_option(self, request_form):
+        pixel_count = PixelCount()
+        for key, value in request_form.iteritems():
+            if key in PixelCount.OPTIONS:
+                pixel_count[key] = value
+        self._options = pixel_count.options()
+    @property
+    def options(self): return self._options
 
