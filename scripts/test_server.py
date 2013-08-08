216568e43e52eab7e2ae1c00c73bfd36a27a95ad
John Peterson <johnp@datalogics.com>
2013-08-08 14:56:41 -0500

    Initial regression test list
    This added a brief list of necessary tests to
    scripts/test_server.py  These need to be implemented
    at minimum.  Additional tests maybe (most likely will be)
    required.

--- scripts/test_server.py
+++ scripts/test_server.py
@@ -1,4 +1,8 @@
 "server regression tests"
+# This file is set to help run regression tests on 
+# the PDFL Web-API server.  This is to be run after
+# changes to the server code to verify no issues 
+# were created with previous code and/or pdf2img
 
 import test_client
 from nose.tools import assert_equal, assert_is_none
@@ -14,4 +18,25 @@ def test_bad_version():
     assert_is_none(response.process_code)
     assert_is_none(response.exc_info)
     assert_is_none(response.output)
+
+# Tests to be made
+# def test_arg_parsing():        #verify arg syntax to pdf2img
+# def test_correct_syntax():     #verify input syntax errors
+# def test_bmp_colormodel():     #verify BMP not RGB or Gray error
+# def test_memory():             #verify insufficient memory error
+# def test_color_model():        #verify color model error
+# def test_output_type():        #verifies out type error
+# def test_no_arguments():       #verifies no arguments error
+# def test_page_range():         #verifies page range error
+# def test_password_not_given(): #verifies password missing error
+# def test_password_incorrect(): #verifes password incorrect error
+# def test_drm_protection():     #verifes DRM protected file error
+# def test_pdf_input():          #verifies input file not a pdf error
+# def test_pdf2img():            #verifies handling of pdf2img crash
+# def test_pdf_file_integrity(): #verifies truncated file error
+# def test_png():                #verify output vs baseline png
+# def test_jpg():                #verify output vs baseline jpg
+# def test_bmp():                #verify output vs baseline bmp
+# def test_tif():                #verify output vs baseline tif
+
 
