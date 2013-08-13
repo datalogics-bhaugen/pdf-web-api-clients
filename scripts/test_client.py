4fbc911ed3bb091c376fbb1f724b0f38a8db3797
Felix Yen <fyen@datalogics.com>
2013-08-13 18:06:34 -0500

    add process_code classes

--- scripts/test_client.py
+++ scripts/test_client.py
@@ -11,11 +11,22 @@ requests_dir = glob.glob(os.path.join(root_dir, 'eggs', 'requests-*.egg'))[0]
 sys.path[0:0] = [requests_dir, os.path.join(root_dir, 'samples', 'python')]
 
 from pdf2img import PDF2IMG
+from pdfclient import ProcessCode
 
 
 API_KEY = 'f54ab5d8-5775-42c7-b888-f074ba892b57'
 BASE_URL = 'http://127.0.0.1:5000'
 VERSION = 0
+
+class StatusCode:
+    OK = 200
+    BadRequest = 400
+    Forbidden = 403
+    NotFound = 404
+    RequestEntityTooLarge = 413
+    UnsupportedMediaType = 415
+    TooManyRequests = 429
+    InternalServerError = 500
 
 def pdf2img(api_key=API_KEY, version=VERSION, base_url=BASE_URL):
     return PDF2IMG(api_key, version, base_url)
