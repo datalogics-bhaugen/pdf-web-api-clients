e7d1a2fc9558a581c8f71987cc0e2fefb4d75762
Felix Yen <fyen@datalogics.com>
2013-08-14 15:05:28 -0500

    refactored to reduce module size

--- src/pdfprocess/pdfprocess/image/argument_parser.py
+++ src/pdfprocess/pdfprocess/image/argument_parser.py
@@ -1,48 +1,7 @@
 "pdfprocess image action arguments"
 
 import argparse
-
-
-class Option(object):
-    def __init__(self, name, normalize=True):
-        self._name = name
-        self._normalize = normalize
-        self._normalized_name = name.lower()
-    def __str__(self):
-        return self._normalized_name if self._normalize else self.name
-    def __eq__(self, other): return self._normalized_name == other.lower()
-    def __ne__(self, other): return not self == other
-    @property
-    def name(self): return self._name
-    @property
-    def action(self): return 'store'
-
-
-class Flag(Option):
-    @property
-    def action(self): return 'store_true'
-
-class FlagAlias(Flag):
-    def __init__(self, name, pdf2img_name):
-        Flag.__init__(self, name)
-        self._pdf2img_name = pdf2img_name.lower()
-    def __str__(self):
-        return self._pdf2img_name
-
-
-OPTIONS = [
-    Flag('OPP', normalize=False),
-    FlagAlias('disableColorManagement', 'noCMM'),
-    FlagAlias('disableThinLineEnhancement', 'noEnhanceThinLines'),
-    FlagAlias('printPreview', 'asPrinted'),
-    FlagAlias('suppressAnnotations', 'noAnnot'),
-    Option('colorModel'),
-    Option('compression'),
-    Option('pages'),
-    Option('password'),
-    Option('pdfRegion'),
-    Option('resolution'),
-    Option('smoothing')]
+from options import Flag, ImageSize, Option, OPTIONS
 
 
 class ImageSize(object):
