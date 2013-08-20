0018905e9a5d9b9457cb3a526a39cf8e5b57b40c
Felix Yen <fyen@datalogics.com>
2013-08-20 13:19:33 -0500

    test 3scale integration directly

--- test/test_3scale.py
+++ test/test_3scale.py
@@ -1,33 +1,40 @@
-# TODO: instantiate pdfprocess.Client directly
-
 "server regression tests, 3scale"
 
 import uuid
 import test
-from test import Result, Test
-from test_client import ProcessCode, StatusCode
+
+from pdfprocess.client import Client
+from pdfprocess.errors import Auth
+from nose.tools import assert_equal, assert_false
 
 
 RATE_LIMITED_ID = 'c953bc0d'
 RATE_LIMITED_KEY = 'c7a7c21fb25c384127879ded5ed3b0a4'
 
 
+class Logger:
+    def error(self, exc):
+        print('error: %s' % exc)
+
+def pdfprocess_client(app_id, app_key):
+    return Client(Logger(), '{"id": "%s", "key": "%s"}' % (app_id, app_key))
+
+
 def test_bad_application_id():
     bad_application_id = str(uuid.uuid4())[:8]
-    _test_bad_application(bad_application_id, test.APPLICATION_KEY)
+    client = pdfprocess_client(bad_application_id, test.APPLICATION_KEY)
+    assert_equal(client.auth(), Auth.Invalid)
 
 def test_bad_application_key():
     bad_application_key = str(uuid.uuid4()).replace('-', '')
-    _test_bad_application(test.APPLICATION_ID, bad_application_key)
+    client = pdfprocess_client(test.APPLICATION_ID, bad_application_key)
+    assert_equal(client.auth(), Auth.Invalid)
 
-def _test_bad_application(application_id, application_key):
-    result = Result(ProcessCode.AuthorizationError, StatusCode.Forbidden)
-    bad_application = Test.pdf2img(application_id, application_key)
-    Test(['data/bad.pdf'], result, bad_application).validate()
-
-def TODO_usage_limit_exceeded():
-    rate_limited_application = Test.pdf2img(RATE_LIMITED_ID, RATE_LIMITED_KEY)
-    result = Result(ProcessCode.InvalidInput, StatusCode.BadRequest)
-    for j in range(20): Test(['data/truncated.pdf'], result).validate()
-    # TODO: ProcessCode.UsageLimitExceeded
+def test_usage_limit_exceeded():
+    client = pdfprocess_client(RATE_LIMITED_ID, RATE_LIMITED_KEY)
+    for j in range(100):
+        auth = client.auth()
+        if auth == Auth.TooFast: return
+        assert_equal(auth, Auth.OK)
+    assert_false('usage limit exceeded')
 
