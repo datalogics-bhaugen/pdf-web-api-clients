6794e2fbb08ce7c53b88f3da43f0f5041d83b771
Felix Yen <fyen@datalogics.com>
2013-10-10 17:18:03 -0500

    accept inputURL as query parameter

--- thumbnail/test/test.py
+++ thumbnail/test/test.py
@@ -10,8 +10,15 @@ parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path[0:0] = [os.path.join(parent_dir, 'thumbnail')]
 from tmpdir import Stdout
 
-def test_thumbnail():
-    thumbnail_url = '127.0.0.1:5050'
-    input_url = 'inputURL=%s' % 'http://www.datalogics.com/pdf/doc/pdf2img.pdf'
-    input = ['curl', '--request', 'GET', '--form', input_url, thumbnail_url]
+
+BASE_URL = '127.0.0.1:5050'
+INPUT_URL = 'inputURL=%s' % 'http://www.datalogics.com/pdf/doc/pdf2img.pdf'
+
+
+def test_form_data_url():
+    input = ['curl', '--request', 'GET', '--form', INPUT_URL, BASE_URL]
+    assert_equal(subprocess.call(input, stdout=Stdout(), stderr=Stdout()), 0)
+
+def test_query_string_url():
+    input = ['curl', '--request', 'GET', '%s?%s' % (BASE_URL, INPUT_URL)]
     assert_equal(subprocess.call(input, stdout=Stdout(), stderr=Stdout()), 0)
