45c2a89af19be6fb9affe69027c8451c876525b9
Felix Yen <fyen@datalogics.com>
2013-10-11 17:46:24 -0500

    use DLENV to determine environment to monitor

--- test/monitor.py
+++ test/monitor.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 
-"usage: monitor.py <input filename> [prod|test]"
+"usage: monitor.py <input filename>"
 
+import os
 import sys
 import test_client
 
 
-DEFAULT_ENVIRONMENT = 'prod'
-
-def input(argv):
-    return argv[1]
-
-def environment(argv):
-    return argv[2] if 2 < len(argv) else DEFAULT_ENVIRONMENT
-
-def base_url(argv):
-    suffix = environment(argv)
-    suffix = '' if suffix == DEFAULT_ENVIRONMENT else '-%s' % suffix
-    return 'https://pdfprocess%s.datalogics-cloud.com' % suffix
+def base_url():
+    environment = (os.getenv('DLENV') or '').lower()
+    environment_suffix = '' if environment == 'prod' else ('-%s' % environment)
+    return 'https://pdfprocess%s.datalogics-cloud.com' % environment_suffix
 
 if __name__ == '__main__':
-    response = test_client.run([input(sys.argv)], base_url(sys.argv))
+    response = test_client.run([sys.argv[1]], base_url())
     if not response: sys.exit(response)
     response.save_image()
     print('created: %s' % response.image_filename)
