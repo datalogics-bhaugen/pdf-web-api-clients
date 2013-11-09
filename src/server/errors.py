9c9b1157f3fd28df74fcc47ba967781acca8ba64
Felix Yen <fyen@datalogics.com>
2013-11-08 18:20:20 -0600

    extend support for jQuery encoding of JSON form values

--- src/server/errors.py
+++ src/server/errors.py
@@ -75,15 +75,14 @@ UNKNOWN = Error(ErrorCode.UnknownError, 'Internal server error',
 
 
 class JSON:
-    @classmethod
-    def get(cls, request_form, part_name, json_name, default_value=''):
+    class RequestFormParser(dict):
         "support jQuery encoding of JSON form values"
-        part = request_form.get(part_name, None)
-        if part is None:
-            jquery_part_name = '{}[{}]'.format(part_name, json_name)
-            return request_form.get(jquery_part_name, default_value)
-        else:
-            return JSON.parse(part).get(json_name, default_value)
+        def parse(self, request_form, part_name):
+            prefix = '{}['.format(part_name)
+            for key, value in request_form.items():
+                if key.startswith(prefix): self[key[len(prefix):-1]] = value
+            self.update(JSON.parse(request_form.get(part_name, '{}')))
+            return self
     @classmethod
     def parse(cls, json):
         try:
