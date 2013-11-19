53fb13e4167d32effdfd393ed1825b67a26e5ff2
Felix Yen <fyen@datalogics.com>
2013-11-19 17:50:58 -0600

    enable brew/coffee route

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -21,7 +21,7 @@ app = flask.Flask('pdfprocess')
 logger.start(app.logger, app.name, Configuration.versions.server)
 
 @app.route('/api/actions/render/pages', methods=['POST'])
-def pdf2img_action():
+def render_pages():
     try:
         return pdf2img.Action.from_request(flask.request)()
     except Error as error:
@@ -39,3 +39,31 @@ def log_traceback():
     for entry in traceback.format_tb(sys.exc_info()[2]):
         logger.error(entry.rstrip())
         if '/eggs/' in entry: return
+
+
+import random
+
+IM_A_TEAPOT = '''<head><title>I'm a teapot</title></head>
+<body>
+    <a href="http://joereddington.com/418-error-code-teapot/">
+        <img src="{0}" alt="{0}" title="{0}">
+    </a>
+    <p>Inspired by Joe Reddington and Laurence O'Toole.&nbsp;
+        <a href="http://creativecommons.org/licenses/by/3.0/">
+            <img src="http://i.creativecommons.org/l/by/3.0/88x31.png"
+                 alt="Creative Commons Attribution 3.0 Unported License"
+                 title="Creative Commons Attribution 3.0 Unported License">
+        </a>
+    </p>
+</body>
+'''
+
+@app.route('/api/actions/brew/coffee', methods=['GET', 'POST'])
+def brew_coffee():
+    csr_base_url = 'http://www.joereddington.com/csr'
+    uploads_base_url = 'http://joereddington.com/wp-content/uploads/2013/09'
+    images = ('{}/pot.png'.format(uploads_base_url),
+              '{}/withtea.png'.format(csr_base_url),
+              '{}/pouring.png'.format(uploads_base_url),
+              '{}/shelf.png'.format(csr_base_url))
+    return IM_A_TEAPOT.format(images[random.randrange(0, len(images))]), 418
