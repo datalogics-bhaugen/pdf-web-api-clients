838ce60f38a719d7199ec75a53a5d25a3db7c51c
Felix Yen <fyen@datalogics.com>
2014-01-21 09:56:32 -0600

    report 'no pdf2img in PATH' error correctly

--- test/mock.py
+++ test/mock.py
@@ -7,9 +7,9 @@ import subprocess
 
 class Client(object):
     def __init__(self, mock, pdf2img='pdf2img'):
-        self._set_pdf2img(pdf2img)
+        self._pdf2img = self._pdf2img_path(pdf2img)
         if not self.pdf2img: sys.exit('no {} in PATH'.format(pdf2img))
-        self._set_temporary_name(pdf2img)
+        self._temporary_name = self._temporary_pdf2img_name(pdf2img)
         subprocess.call(['mv', self.pdf2img, self.temporary_name])
         subprocess.call(['ln', '-s', os.path.abspath(mock), self.pdf2img])
     def __del__(self):
@@ -18,15 +18,14 @@ class Client(object):
         return self
     def __exit__(self, type, value, traceback):
         pass
-    def _set_pdf2img(self, pdf2img):
+    def _pdf2img_path(self, pdf2img):
         for dir in os.environ['PATH'].split(os.pathsep):
             filename = os.path.join(dir, pdf2img)
             if os.path.isfile(filename) and os.access(filename, os.X_OK):
-                self._pdf2img = filename
-    def _set_temporary_name(self, pdf2img):
+                return filename
+    def _temporary_pdf2img_name(self, pdf2img):
         root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        pdf2img_basename = os.path.basename(pdf2img)
-        self._temporary_name = os.path.join(root_dir, 'bin', pdf2img_basename)
+        return os.path.join(root_dir, 'bin', os.path.basename(pdf2img))
     @property
     def pdf2img(self): return self._pdf2img
     @property
