0a0c579cdf286e70994c681548664386909190d1
Colleen Stock <cstock@datalogics.com>
2014-10-31 08:58:27 -0500

    updates to sample javascript client

--- samples/javascript/App1/App1/js/default.js
+++ samples/javascript/App1/App1/js/default.js
@@ -1,58 +1,64 @@
-﻿var uriString = new Windows.Foundation.Uri("https://pdfprocess.datalogics.com/api/actions/decorate/document");
-
+﻿var uriString = new Windows.Foundation.Uri("http://10.2.4.204:8080/api/decorate/document");
+//192.168.132.1
 var httpClient = new Windows.Web.Http.HttpClient();
-var httpPromise;
-var temp;
 
-httpClient.defaultRequestHeaders.append("user-agent", "python-requests/2.0.1 CPython/2.7.5 Darwin/13.0.0");
-httpClient.defaultRequestHeaders.append("Accept", "*/*");
-httpClient.defaultRequestHeaders.append("Accept-Encoding", "gzip, deflate, compress");
-var requestContent = new Windows.Web.Http.HttpMultipartFormDataContent();
+setTimeout(function () { Debug.writeln("Yo") }, 30000);
+addContent(doPost);
 
-requestContent.add(new Windows.Web.Http.HttpStringContent(
-    "{\"id\":\"INSERT ID HERE!!!\", \"key\":\"INSERT KEY HERE!!!\"}"), "application");
+function addContent(callback) {
+    Debug.writeln("Adding content");
+    httpClient.defaultRequestHeaders.append("user-agent", "python-requests/2.0.1 CPython/2.7.5 Darwin/13.0.0");
+    httpClient.defaultRequestHeaders.append("Accept", "*/*");
+    httpClient.defaultRequestHeaders.append("Accept-Encoding", "gzip, deflate, compress");
+    var requestContent = new Windows.Web.Http.HttpMultipartFormDataContent();
 
-/*temp = new Windows.Storage.StorageFile.getFileFromPathAsync(
-    "C:\Users\cstock\Documents\ScenariosOfWorkingWithDLE.pdf").done.then(function (file) {
-        requestContent.add(new Windows.Web.Http.HttpStreamContent(file.openReadAsync), "input");
-    });*/
+    requestContent.add(new Windows.Web.Http.HttpStringContent(
+        "{\"id\":\"84445ec0\", \"key\":\"2d3eac77bb3b9bea69a91e625b9241d2\"}"), "application");
 
-var folderPicker = new Windows.Storage.Pickers.FolderPicker();
-folderPicker.fileTypeFilter.replaceAll(["*"]);
-folderPicker.pickSingleFolderAsync("C:\Users\cstock\Documents\Visual Studio 14\Projects\\").then(function (allowedFolder) {
-    
-    temp = allowedFolder.getFileAsync("ScenariosOfWorkingWithDLE.pdf").then(function (pdfFile) {
-        pdfFile.openReadAsync().then(function (pdfData) {
-            requestContent.add(new Windows.Web.Http.HttpStreamContent(pdfData), "input");
+    var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+    folderPicker.fileTypeFilter.replaceAll(["*"]);
+    folderPicker.pickSingleFolderAsync("C:\Users\cstock\Documents\Visual Studio 14\Projects\\").then(function (allowedFolder) {
 
+        var temp = allowedFolder.getFileAsync("ScenariosOfWorkingWithDLE.pdf").then(function (pdfFile) {
+            pdfFile.openReadAsync().then(function (pdfData) {
+                        requestContent.add(new Windows.Web.Http.HttpStreamContent(pdfData), "input");
 
-            Debug.writeln("Yello?");
-        });
+                        Debug.writeln("Yello?");
+                            
+                        var temp2 = allowedFolder.getFileAsync("AllPages.xml").then(function (decoFile) {
+                            decoFile.openReadAsync().then(function (decoData) {
+                                    requestContent.add(new Windows.Web.Http.HttpStreamContent(decoData), "decorationData");
+
+                                    Debug.writeln("Yello dos");
+                                    requestContent.readAsStringAsync().then(function (requestData) {
+                                        Debug.writeln(requestData);
+
+                                        callback(requestContent);
+                                    });
+                            });
+                    });
+                });
+            });
     });
 
-    temp2 = allowedFolder.getFileAsync("AllPages.xml").then(function (decoFile) {
-        decoFile.openReadAsync().then(function (decoData) {
-            requestContent.add(new Windows.Web.Http.HttpStreamContent(decoData), "decorationData");
+}
 
-            doPost();
-        });
-    });
+function doPost(requestContent) {
 
-});
-
-function doPost() {
     Debug.writeln("Posting");
-    httpPromise = httpClient.postAsync(uriString, requestContent).then(function (response) {
+
+    var httpPromise = httpClient.postAsync(uriString, requestContent).then(function (response) {
         outputStatus = response.statusCode + " " + response.reasonPhrase;
+        Debug.writeln("response");
         Debug.writeln(outputStatus);
 
         response.content.readAsStringAsync().then(function (responseBodyAsText) {
-            // Format the HTTP response to display better
+            //Format the HTTP response to display better
             responseBodyAsText = responseBodyAsText.replace(/<br>/g, "\r\n");
 
             Debug.writeln(responseBodyAsText);
             return response;
-
         });
-    });
+    })
 }
+
